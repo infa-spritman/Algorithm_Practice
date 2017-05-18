b0914c63fd9040e0c5364b723dabeb9e220a361c
Sushant Pritmani <whisprrind.twister@gmail.com>
2017-05-18 17:24:19 -0400

    Arrays:Leader Problem http://www.geeksforgeeks.org/leaders-in-an-array/

---
+++ src/DS/arrays/LeadersArray.java
@@ -0,0 +1,34 @@
+package DS.arrays;
+
+
+/**
+ * Created by Sushant on 5/18/2017.
+ */
+public class LeadersArray {
+
+    private static int[] findLeader(int[] arr, int size) {
+
+        int max = arr[size - 1];
+        int[] tempArray = new int[size];
+        int position = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (arr[i] >= max) {
+                tempArray[position] = arr[i];
+                max = arr[i];
+                position++;
+            }
+
+        }
+        return tempArray;
+    }
+
+    public static void main(String[] args) {
+        int[] input_array = {16, 17, 4, 3, 5, 2};
+        int size = input_array.length;
+        for (int i : findLeader(input_array, size)) {
+            if (i != 0)
+                System.out.println(i);
+        }
+
+    }
+}
