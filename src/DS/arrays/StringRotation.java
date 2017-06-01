25a7c313edbd929dbd6bbdbb942b391a6cace9ee
Sushant Pritmani <whisprrind.twister@gmail.com>
2017-06-01 16:08:02 -0400

    String Rotation CCI: 1.8

--- src/DS/arrays/StringRotation.java
+++ src/DS/arrays/StringRotation.java
@@ -5,7 +5,19 @@ package DS.arrays;
  */
 public class StringRotation {
 
+    public static boolean isRotate(String s1, String s2) {
 
-    public static void
+        StringBuilder sb = new StringBuilder();
+        sb.append(s1).append(s1);
+        if (sb.indexOf(s2) != -1)
+            return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(isRotate("bottleerwat", "waterbottle"));
+    }
 }
 
